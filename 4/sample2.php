bd4a097fa5e6d23c50248ff0dade82bb1a827527
muraki_111 <60808536+muraki111@users.noreply.github.com>
2020-10-11 22:08:09 +0900

    完成

---
+++ 4/sample2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <?php
+    echo "        <title>ページタイトル</title>\n";
+    ?>
+</head>
+<?php
+echo <<<EOT
+    <body>
+        <p> こんにちは php の世界へ！ </p>
+    </body>
+EOT;
+?>
+</html>
