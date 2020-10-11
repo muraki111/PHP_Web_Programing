d2a9894c814aad85b97cae3020076a5be385c20c
muraki_111 <60808536+muraki111@users.noreply.github.com>
2020-10-11 20:50:52 +0900

    変数追加

--- 4/sample1.php
+++ 4/sample1.php
@@ -7,7 +7,28 @@
     </head>
     <body>
 <?php
-$a='<p>red</p>';
+    $a='<p>red</p>';
+    $b='Can\'t';
+    $c='it is $a';
+    $d="it is $a";
+    $e="it is \$a";
+    $f='it is '.$a;
+    $g=  $a.$b ;
+    $h= '$a.$b';
+    $i= "$a.$b";
+    $j='\\';
+    $k="a=\"hogehoge\"";
+    $l="hogehoge='$a';";
+    $m='hogehoge="$a";';
+    $n='hogehoge='.';';
+    $o="\".".".\"";
+    $p="hogehoge='.';";
+    $q='hogehoge=\'.\';';
+    $r= <<< EOT
+    <p> hogehoge </p>
+    $a
+    <p> hogehoge </p>
+    EOT;
 ?>
         <p> $a は 「<?php echo $a ?>」です。 </p>
     </body>
