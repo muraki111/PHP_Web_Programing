2dd9870d61d53d75250779c3d4238b420b57f410
muraki_111 <60808536+muraki111@users.noreply.github.com>
2020-10-13 22:19:21 +0900

    first commit

---
+++ 5/sample3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<?php
+    $price['apple'] = 150;
+    $price['banana'] = 120;
+    $price['pinapple'] = 300;
+?>
+    <head>
+        <meta charset="UTF-8" />
+        <title>ページタイトル</title>
+    </head>
+    <body>
+    <p>
+<?php
+
+?>
+    </p>
+    </body>
+</html>
