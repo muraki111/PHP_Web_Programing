8125572e02f07332ce2e25d4a0a97d0a5d6935b4
muraki_111 <60808536+muraki111@users.noreply.github.com>
2020-10-13 23:04:01 +0900

    リンゴ完成

--- 5/sample3.php
+++ 5/sample3.php
@@ -1,19 +1,30 @@
 <!DOCTYPE html>
 <html>
-<?php
-    $price['apple'] = 150;
-    $price['banana'] = 120;
-    $price['pinapple'] = 300;
-?>
     <head>
         <meta charset="UTF-8" />
         <title>ページタイトル</title>
     </head>
     <body>
-    <p>
-<?php
 
+
+<?php
+if(isset($_GET['n1'])){
+   $n1=$_GET['n1'];
+}else{
+   $n1=1;
+}
+if(isset($_GET['name1'])){
+   $name1=$_GET['name1'];
+}else{
+   $name1="りんご";
+}
+
+    echo "<p> $name1 : $n1</p>";
+
+echo '<a href="http://127.0.0.1:10800/~sspuser/5/sample3.php?n1='. ($n1+1) . '&name1=' . $name1 . '">リンゴを1個追加</a> ';
 ?>
-    </p>
+        <a href="http://127.0.0.1:10800/~sspuser/5/sample3.php">戻る</a>
+
+
     </body>
 </html>
