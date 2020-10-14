87630a37d66165840f9b12703bcf29f03a8cefc0
muraki_111 <60808536+muraki111@users.noreply.github.com>
2020-10-14 14:25:23 +0900

    form基礎コピペ

---
+++ 6/testfile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>ページタイトル</title>
+    </head>
+    <body>
+        <h1> フォームのサンプル </h1>
+        <form method="get" action="recv.php">
+            入力1<input type="text" name="Txt1" value="">
+            <br>
+            入力2<textarea name="Txt2" rows="5" cols="30"></textarea>
+            <br>
+            <button type="submit" name="Btn1" value="Btn1">ボタン1</button>
+        </form>
+    </body>
+</html>
