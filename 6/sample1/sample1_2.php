ad3f3d06998f6dd31f4e8a1c4578e894b9b748d4
muraki_111 <60808536+muraki111@users.noreply.github.com>
2020-10-17 18:10:46 +0900

    投稿画面レイアウト完成

--- 6/sample1/sample1_2.php
+++ 6/sample1/sample1_2.php
@@ -7,7 +7,8 @@
     <body>
         <h1 align="center"> なんちゃって掲示板 </h1>
         <form method="get" action="sample1".php">
-        <button type="submit" name="Btn1" value="Btn1"style="width:100%;height:50px">新規投稿</button>
+        <textarea name="Txt2" rows="5" cols="30" style="width:100%;height:300px"></textarea>
+        <button type="submit" name="Btn1" value="Btn1"style="width:100%;height:50px">投稿</button>
         </form>
     </body>
 </html>
