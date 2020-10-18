05a2f720e9fd58227181e92550612682de554de4
muraki_111 <60808536+muraki111@users.noreply.github.com>
2020-10-18 12:49:37 +0900

    完成

--- 6/sample2/sample2_1.php
+++ 6/sample2/sample2_1.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <h1 align="center"> なんちゃって掲示板 </h1>
-        <form method="post" action="sample1_2.php">
+        <form method="post" action="sample2_2.php">
         <button type="submit" name="Btn1" value="Btn1"style="width:100%;height:50px">新規投稿</button>
         </form>
     </body>
