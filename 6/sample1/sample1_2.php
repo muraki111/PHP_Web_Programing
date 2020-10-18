77c4ec473c592f7a38aea145bf4018e902f7fbf5
muraki_111 <60808536+muraki111@users.noreply.github.com>
2020-10-18 12:43:42 +0900

    完成

--- 6/sample1/sample1_2.php
+++ 6/sample1/sample1_2.php
@@ -6,9 +6,9 @@
     </head>
     <body>
         <h1 align="center"> なんちゃって掲示板 </h1>
-        <form method="get" action="sample1".php">
-        <textarea name="Txt2" rows="5" cols="30" style="width:100%;height:300px"></textarea>
-        <button type="submit" name="Btn1" value="Btn1"style="width:100%;height:50px">投稿確認</button>
+        <form method="post" action="sample1_3.php">
+        <textarea name="Txt" rows="5" cols="30" style="width:100%;height:300px"></textarea>
+        <button type="submit" name="Btn2" value="Btn2"style="width:100%;height:50px">投稿確認</button>
         </form>
     </body>
 </html>
--- 6/sample1/sample1_3.php
+++ 6/sample1/sample1_3.php
@@ -6,8 +6,17 @@
     </head>
     <body>
         <h1 align="center"> なんちゃって掲示板 </h1>
-        <form method="get" action="sample1".php">
-        <button type="submit" name="Btn1" value="Btn1"style="width:100%;height:50px">投稿する</button>
+        <h2 align="center">入力内容は</h2>
+        <table border="1" align="center">
+            <?php
+                $a=$_POST['Txt'];
+                echo "<td>";
+                echo htmlspecialchars($a) . '<br>';
+                echo "</td>";
+            ?>
+        </table>
+        <form method="post" action="sample1_1.php">
+        <button type="submit" name="Btn3" value="Btn3"style="width:100%;height:50px">投稿する</button>
         </form>
     </body>
 </html>
