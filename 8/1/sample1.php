515a229b8b26caaf1acdb6bb426aceaaac7dcc6b
muraki_111 <60808536+muraki111@users.noreply.github.com>
2020-10-23 01:12:57 +0900

    完成

--- 8/1/sample1.php
+++ 8/1/sample1.php
@@ -10,22 +10,22 @@ $hostname = '127.0.0.1';
 $username = 'root';
 $password = 'dbpass';
 
-$dbname = 'kadai8kadai';
-$tablename = 'kadai8kadaitb';
+$dbname = 'sample8_db';
+$tablename = 'sample8_1';
 
 $link = mysqli_connect($hostname,$username,$password);
 if(! $link){ exit("Connect error!"); }
 
-$result = mysqli_query($link,"CREATE DATABASE kadai8kadai CHARACTER SET utf8");
-if(!$result) { echo "Create database kadai8kadai failed!\n"; }
+$result = mysqli_query($link,"CREATE DATABASE sample8_db CHARACTER SET utf8");
+if(!$result) { echo "Create database sample8_db failed!\n"; }
 
-$result = mysqli_query($link,"USE kadai8kadai");
+$result = mysqli_query($link,"USE sample8_db");
 if(!$result) { exit("USE failed!"); }
 
-$result = mysqli_query($link,"CREATE TABLE kadai8kadaitb(day DATETIME, price INT(11), deposit BLOB,withdraw BLOB) CHARACTER SET utf8");
-if(!$result) { echo "Create database kadai8kadaitb failed!\n"; }
+$result = mysqli_query($link,"CREATE TABLE sample8_1(Name INT, Day DATE, price INT, Weight INT) CHARACTER SET utf8");
+if(!$result) { echo "Create database sample8_db failed!\n"; }
 
-$result = mysqli_query($link,"INSERT INTO kadai8kadaitb (day,price,deposit,withdraw) VALUES('1999-11-18 23:59:24',2500,'YES','NO') ,('2000-03-04 15:16:17',90000,'NO','YES') ,('2000-05-06 17:28:32', 12000,'NO','YES') ,('2000-05-06 17:28:32', 12000,'NO','YES')");
+$result = mysqli_query($link,"INSERT INTO sample8_1 (Name,Day,Price,Weight) VALUES(3,'2020-10-01',210,15) ,(3,'2020-10-01',210,15) ,(4,'2020-10-01',110,10) ,(5,'2020-10-01',500,20),(4,'2020-10-01',110,10)");
 if(!$result){ exit("INSERT error!"); }
 
 echo "Create db and table and update succeeded!\n";
