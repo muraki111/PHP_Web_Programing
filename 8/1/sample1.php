4cd9057b57668b363e21bae84037fed8d9b27b22
muraki_111 <60808536+muraki111@users.noreply.github.com>
2020-10-22 22:56:16 +0900

    slack to rikuriver2

--- 8/1/sample1.php
+++ 8/1/sample1.php
@@ -10,30 +10,27 @@ $hostname = '127.0.0.1';
 $username = 'root';
 $password = 'dbpass';
 
-$dbname = 'kadai8-1';
-$tablename = 'kadai8tb-1';
+$dbname = 'kadai8kadai';
+$tablename = 'kadai8kadaitb';
 
 $link = mysqli_connect($hostname,$username,$password);
 if(! $link){ exit("Connect error!"); }
 
-$result = mysqli_query($link,"CREATE DATABASE $dbname CHARACTER SET utf8");
-if(!$result) { echo "Create database $dbname failed!\n"; }
+$result = mysqli_query($link,"CREATE DATABASE kadai8kadai CHARACTER SET utf8");
+if(!$result) { echo "Create database kadai8kadai failed!\n"; }
 
-$result = mysqli_query($link,"USE $dbname");
+$result = mysqli_query($link,"USE kadai8kadai");
 if(!$result) { exit("USE failed!"); }
 
-$result = mysqli_query($link,"INSERT datetime day=19991118235924, int price=2500, blob deposit='YES', blob withdraw='NO'");
-if(! $result){ exit("INSERT error!"); }
-$result = mysqli_query($link,"INSERT datetime day=20000304151617, int price=90000, blob deposit='NO', blob withdraw='YES'");
-if(! $result){ exit("INSERT error!"); }
-$result = mysqli_query($link,"INSERT datetime day=20000506172832, int price=12000, blob deposit='NO', blob withdraw='YES'");
-if(! $result){ exit("INSERT error!"); }
-$result = mysqli_query($link,"INSERT datetime day=20011118192523, int price=100000, blob deposit='YES', blob withdraw='NO'");
-if(! $result){ exit("INSERT error!"); }
+$result = mysqli_query($link,"CREATE TABLE kadai8kadaitb(day DATETIME, price INT(11), deposit BLOB,withdraw BLOB) CHARACTER SET utf8");
+if(!$result) { echo "Create database kadai8kadaitb failed!\n"; }
+
+$result = mysqli_query($link,"INSERT INTO kadai8kadaitb (day,price,deposit,withdraw) VALUES('1999-11-18 23:59:24',2500,'YES','NO') ,('2000-03-04 15:16:17',90000,'NO','YES') ,('2000-05-06 17:28:32', 12000,'NO','YES') ,('2000-05-06 17:28:32', 12000,'NO','YES')");
+if(!$result){ exit("INSERT error!"); }
 
 echo "Create db and table and update succeeded!\n";
 
 mysqli_close($link);
 ?>
- </body>
+</body>
 </html>
