1debb58a8e8bc0febdc66f074f58965812ed13a4
muraki_111 <60808536+muraki111@users.noreply.github.com>
2020-11-01 21:34:50 +0900

    function page_list 完成

--- 10/sample2.php
+++ 10/sample2.php
@@ -33,5 +33,58 @@ else
 {
     echo "Internal Error!"; // あり得ないエラー
 }
+function echo_page_list()
+{
+    global $hostname, $username, $password, $dbname, $tablename;
 
+    echo <<<EOT
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>トップページ</title>
+    </head>
+    <body>
+EOT;
+
+    $link = mysqli_connect($hostname,$username,$password,$dbname);
+    if (! $link){ exit("Connect error!"); }
+
+    $result = mysqli_query($link,"select * from $tablename");
+    if (!$result){ exit("Unexpected query error!"); }
+
+    echo '<table border="1">';
+
+    $ary_of_fieldinfo = mysqli_fetch_fields($result);
+
+    foreach($ary_of_fieldinfo as $key => $value)
+    {
+        echo "<th>" . htmlspecialchars($value->name) . "</th>";
+    }
+
+    while ($row = mysqli_fetch_row($result))
+    {
+        echo "<tr>";
+        foreach($row as $key => $value)
+        {
+            echo "<td>" . htmlspecialchars($value) . "</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+    mysqli_free_result($result);
+
+    mysqli_close($link);
+
+    echo <<<EOT
+        <form method="post" action="sample.php">
+            <button type="submit" name="button_new" value="button_new">新規入力</button>
+            <input type="hidden" name="transition" value="trans_input_new">
+        </form>
+EOT;
+    echo <<<EOT
+    </body>
+</html>
+EOT;
+}
 ?>
