ed5411a90427a18c5903811e4d534513b2e0ea08
muraki111 <*****@users.noreply.github.com>
2020-11-03 19:42:46 +0900

    first commit (ユーザ認証画面)

---
+++ 11/sample1.php
@@ -0,0 +1,54 @@
+<?php
+$passlist=array( 'hogehoge' => 'hogepass', 'hoge2' => 'hoge2pass');
+
+if(!isset($_POST['user'])){
+    echo_auth_page("ログイン");
+    exit;
+}
+$user=$_POST['user'];
+$pass=$_POST['pass'];
+
+if( (!isset($passlist[$user])) || $passlist[$user] != $pass){
+    echo_auth_page("パスワードが違います");
+    exit;
+}
+
+echo_hello_page($user);
+
+
+
+function echo_auth_page($msg){
+echo <<<EOT
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>ページタイトル</title>
+    </head>
+    <body>
+$msg
+    <form method="POST" action="sample1.php">
+        username <input type="text" name="user" value=""><br>
+        password <input type="password" name="pass" value=""><br>
+        <button type="submit" name="login" value="login">Login</button>
+    </form>
+    </body>
+</html>
+EOT;
+}
+
+function echo_hello_page($who){
+echo <<<EOT
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>ページタイトル</title>
+    </head>
+    <body>
+こんにちは $who さん
+    </body>
+</html>
+EOT;
+}
+?>
