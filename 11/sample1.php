fdfebab879998c7050cbeafdc2c6156dd3be8ad9
muraki111 <*****@users.noreply.github.com>
2020-11-03 21:25:04 +0900

    完成提出

--- 11/sample1.php
+++ 11/sample1.php
@@ -1,22 +1,31 @@
 <?php
-$passlist=array( 'tcu' => 'tcupass', 'tcu2' => 'tcu2pass');
+$passlist=array( 'hogehoge' => 'hogepass', 'hoge2' => 'hoge2pass');
 
-if(!isset($_POST['user'])){
+if(!isset($_POST['user']))
+{
     echo_auth_page("ログイン");
     exit;
 }
 $user=$_POST['user'];
 $pass=$_POST['pass'];
 
-if( (!isset($passlist[$user])) || $passlist[$user] != $pass){
+if( (!isset($passlist[$user])) || $passlist[$user] != $pass)
+{
     echo_auth_page("パスワードが違います");
     exit;
 }
 
+if (isset($_POST['mypage']))
+{
+    echo_mypage_page($user);
+    exit;
+}
 echo_hello_page($user);
 
 
-function echo_auth_page($msg){
+////////////////////////////////////////////////////////////////////////
+function echo_auth_page($msg)
+{
 echo <<<EOT
 <!DOCTYPE html>
 <html>
@@ -35,19 +44,35 @@ $msg
 </html>
 EOT;
 }
-
-function echo_hello_page($who){
+////////////////////////////////////////////////////////////////////////
+function echo_hello_page($who)
+{
+global $user ,$pass;
 echo <<<EOT
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8" />
-        <title>ページタイトル</title>
-    </head>
-    <body>
 こんにちは $who さん
-    </body>
-</html>
+<form method="POST" action="sample1.php">
+        <button type="submit" name="mypage" value="mypage">マイページ</button>
+        <input type="hidden" name="user" value="$user">
+        <input type="hidden" name="pass" value="$pass">
+</form>
+EOT;
+}
+function echo_mypage_page($who)
+{
+    global $user , $pass;
+    echo <<<EOT
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8" />
+            <title>ページタイトル</title>
+        </head>
+        <body>
+    $who さんのマイページ
+    <form method="POST" action="sample1.php">
+        <button type="submit" name="mypage" value="mypage">ログアウト</button>
+        </body>
+    </html>
 EOT;
 }
 ?>
